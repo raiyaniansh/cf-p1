fef2fc42e9d07c32bbcef58196ecaabbc2d285a0
ansh <anshraiyani009@gmail.com>
2022-11-29 08:04:56 +0530

    29/11/2022

---
+++ android/app/src/main/kotlin/com/example/p1hello/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.p1hello
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text("Flutter day 1"),centerTitle: true,
+          backgroundColor: Colors.red,
+        ),
+        body: Center(
+          child: Text(
+              "Hello\n\n\n\nworld",
+          style: TextStyle(fontSize: 25,color: Colors.red,fontWeight: FontWeight.w500,letterSpacing: 4,decoration: TextDecoration.underline,decorationColor: Colors.yellow[600]),
+        ),
+      ),
+    )
+  )
+  );
+}
